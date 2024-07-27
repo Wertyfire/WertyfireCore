34fd3c652377f6c38d1462016de16de27df08420
Wertyfire <sw405@yandex.ru>
2024-07-27 17:25:14 +0300

    Changed access modifier

--- src/main/java/ru/wertyfiregames/wertyfirecore/WertyfireCoreMod.java
+++ src/main/java/ru/wertyfiregames/wertyfirecore/WertyfireCoreMod.java
@@ -8,14 +8,8 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.*;
 import cpw.mods.fml.common.ModMetadata;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.material.MapColor;
-import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
 import org.apache.logging.log4j.Logger;
-import ru.wertyfiregames.wertyfirecore.block.DefaultBlock;
 import ru.wertyfiregames.wertyfirecore.context.IMod;
-import ru.wertyfiregames.wertyfirecore.context.ModContext;
 import ru.wertyfiregames.wertyfirecore.util.Version;
 
 @Mod(modid = WertyfireCoreMod.MOD_ID, name = WertyfireCoreMod.NAME, version = WertyfireCoreMod.VERSION)
@@ -26,9 +20,9 @@ public class WertyfireCoreMod implements IMod {
     protected static final String buildNum = "01";
 
     @Instance
-    public WertyfireCoreMod INSTANCE;
+    public static WertyfireCoreMod INSTANCE;
     @Metadata
-    public ModMetadata METADATA;
+    public static ModMetadata METADATA;
 
     private static Logger modLogger;
 
